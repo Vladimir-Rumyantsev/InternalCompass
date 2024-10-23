5e2d116c9af6a67094e9be90e2af3241f38e03d8
Vladimir-Rumyantsev <113893528+Vladimir-Rumyantsev@users.noreply.github.com>
2024-10-23 10:49:25 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import datetime
 import time
-from data import *
+import data
 
 ex_errors = 0
 
@@ -60,118 +60,169 @@ class User:
 
 
 def telegram_bot():
-    @bot.message_handler(commands=['start'])
+    @data.bot.message_handler(commands=['start'])
     def command_start(message):
         global ex_errors
         ex_errors = 0
         user = User(message.chat.id, dont_remember_mode=True)
-        bot.send_message(message.chat.id, welcome_text, reply_markup=where_to_start_markup, parse_mode='Markdown')
+        with open(data.photo_path, 'rb') as photo:
+            data.bot.send_photo(
+                message.chat.id,
+                photo,
+                caption=data.welcome_text,
+                reply_markup=data.where_to_start_markup,
+                parse_mode='Markdown'
+            )
         user.mode = 0
         user.write()
 
-    @bot.callback_query_handler(func=lambda call: True)
+    @data.bot.callback_query_handler(func=lambda call: True)
     def callback_handler(call):
         global ex_errors
         ex_errors = 0
         user = User(call.message.chat.id)
 
-        if (((user.mode == 0) and (call.data == where_to_start_button.callback_data)) or
-                (call.data == section_selection_button.callback_data)):
-            bot.send_message(
+        if (((user.mode == 0) and (call.data == data.where_to_start_button.callback_data)) or
+                (call.data == data.main_button.callback_data)):
+            data.bot.send_message(
                 call.message.chat.id,
-                main_menu,
-                reply_markup=sections_for_training_markup,
+                data.main_menu,
+                reply_markup=data.sections_for_training_markup,
                 parse_mode='Markdown'
             )
             user.mode = 1
 
         elif user.mode == 1:
             match call.data:
-                case sport_button.callback_data:
-                    bot.send_message(call.message.chat.id, sport_text,
-                                     reply_markup=benefit_markup, parse_mode='Markdown')
+                case data.sport_button.callback_data:
+                    data.bot.send_message(
+                        call.message.chat.id,
+                        data.sport_text,
+                        reply_markup=data.benefit_markup,
+                        parse_mode='Markdown'
+                    )
                     user.mode = 2
-                case nutrition_button.callback_data:
-                    bot.send_message(call.message.chat.id, nutrition_text,
-                                     reply_markup=benefit_markup, parse_mode='Markdown')
+                case data.nutrition_button.callback_data:
+                    data.bot.send_message(
+                        call.message.chat.id,
+                        data.nutrition_text,
+                        reply_markup=data.benefit_markup,
+                        parse_mode='Markdown'
+                    )
                     user.mode = 3
-                case sleep_button.callback_data:
-                    bot.send_message(call.message.chat.id, sleep_text,
-                                     reply_markup=benefit_markup, parse_mode='Markdown')
+                case data.sleep_button.callback_data:
+                    data.bot.send_message(
+                        call.message.chat.id,
+                        data.sleep_text,
+                        reply_markup=data.benefit_markup,
+                        parse_mode='Markdown'
+                    )
                     user.mode = 4
-                case meditation_button.callback_data:
-                    bot.send_message(call.message.chat.id, meditation_text,
-                                     reply_markup=benefit_markup, parse_mode='Markdown')
+                case data.meditation_button.callback_data:
+                    data.bot.send_message(
+                        call.message.chat.id,
+                        data.meditation_text,
+                        reply_markup=data.benefit_markup,
+                        parse_mode='Markdown'
+                    )
                     user.mode = 5
-                case relationships_button.callback_data:
-                    bot.send_message(call.message.chat.id, relationships_text,
-                                     reply_markup=benefit_markup, parse_mode='Markdown')
+                case data.relationships_button.callback_data:
+                    data.bot.send_message(
+                        call.message.chat.id,
+                        data.relationships_text,
+                        reply_markup=data.benefit_markup,
+                        parse_mode='Markdown'
+                    )
                     user.mode = 6
-                case work_button.callback_data:
-                    bot.send_message(call.message.chat.id, work_text,
-                                     reply_markup=benefit_markup, parse_mode='Markdown')
+                case data.work_button.callback_data:
+                    data.bot.send_message(
+                        call.message.chat.id,
+                        data.work_text,
+                        reply_markup=data.benefit_markup,
+                        parse_mode='Markdown'
+                    )
                     user.mode = 7
-                case hobby_button.callback_data:
-                    bot.send_message(call.message.chat.id, hobby_text,
-                                     reply_markup=benefit_markup, parse_mode='Markdown')
+                case data.hobby_button.callback_data:
+                    data.bot.send_message(
+                        call.message.chat.id,
+                        data.hobby_text,
+                        reply_markup=data.benefit_markup,
+                        parse_mode='Markdown'
+                    )
                     user.mode = 8
-                case relaxation_button.callback_data:
-                    bot.send_message(call.message.chat.id, relaxation_text,
-                                     reply_markup=benefit_markup, parse_mode='Markdown')
+                case data.relaxation_button.callback_data:
+                    data.bot.send_message(
+                        call.message.chat.id,
+                        data.relaxation_text,
+                        reply_markup=data.benefit_markup,
+                        parse_mode='Markdown'
+                    )
                     user.mode = 9
-                case other_protocols_button.callback_data:
-                    bot.send_message(call.message.chat.id, protocols_text,
-                                     reply_markup=section_selection_markup, parse_mode='Markdown')
+                case data.other_protocols_button.callback_data:
+                    data.bot.send_message(
+                        call.message.chat.id,
+                        data.protocols_text,
+                        reply_markup=data.main_markup,
+                        parse_mode='Markdown'
+                    )
                     user.mode = 10
 
-        elif 2 <= user.mode <= 9:
+        elif (2 <= user.mode <= 9) and (call.data == data.benefit_button.callback_data):
             response_text: str = ''
             match user.mode:
                 case 2:
-                    response_text = sport_benefits
+                    response_text = data.sport_benefits
                 case 3:
-                    response_text = nutrition_benefits
+                    response_text = data.nutrition_benefits
                 case 4:
-                    response_text = sleep_benefits
+                    response_text = data.sleep_benefits
                 case 5:
-                    response_text = meditation_benefits
+                    response_text = data.meditation_benefits
                 case 6:
-                    response_text = relationships_benefits
+                    response_text = data.relationships_benefits
                 case 7:
-                    response_text = work_benefits
+                    response_text = data.work_benefits
                 case 8:
-                    response_text = hobby_benefits
+                    response_text = data.hobby_benefits
                 case 9:
-                    response_text = relaxation_benefits
-            bot.send_message(call.message.chat.id, response_text,
-                             reply_markup=how_to_do_it_markup, parse_mode='Markdown')
+                    response_text = data.relaxation_benefits
+            data.bot.send_message(
+                call.message.chat.id,
+                response_text,
+                reply_markup=data.how_to_do_it_markup,
+                parse_mode='Markdown'
+            )
             user.mode += 8
 
-        elif 10 <= user.mode <= 17:
+        elif (10 <= user.mode <= 17) and (call.data == data.how_to_do_it_button.callback_data):
             response_text: str = ''
             match user.mode:
                 case 10:
-                    response_text = sport_how_to_do_it
+                    response_text = data.sport_how_to_do_it
                 case 11:
-                    response_text = nutrition_how_to_do_it
+                    response_text = data.nutrition_how_to_do_it
                 case 12:
-                    response_text = sleep_how_to_do_it
+                    response_text = data.sleep_how_to_do_it
                 case 13:
-                    response_text = meditation_how_to_do_it
+                    response_text = data.meditation_how_to_do_it
                 case 14:
-                    response_text = relationships_how_to_do_it
+                    response_text = data.relationships_how_to_do_it
                 case 15:
-                    response_text = work_how_to_do_it
+                    response_text = data.work_how_to_do_it
                 case 16:
-                    response_text = hobby_how_to_do_it
+                    response_text = data.hobby_how_to_do_it
                 case 17:
-                    response_text = relaxation_how_to_do_it
-            bot.send_message(call.message.chat.id, response_text,
-                             reply_markup=section_selection_markup, parse_mode='Markdown')
+                    response_text = data.relaxation_how_to_do_it
+            data.bot.send_message(
+                call.message.chat.id,
+                response_text,
+                reply_markup=data.main_markup,
+                parse_mode='Markdown'
+            )
 
         user.write()
 
-    bot.polling()
+    data.bot.polling()
 
 
 if __name__ == '__main__':
